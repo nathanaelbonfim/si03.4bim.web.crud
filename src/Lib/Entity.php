a28c0485b6e5cbbfeec6424a16fb72f72c192be2
Nathanael <dev@nathabonfim59.com>
2022-12-02 05:32:24 -0300

    feat(framework): entidade e table

--- src/Lib/Entity.php
+++ src/Lib/Entity.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace PHPhademic\Lib;
+
+use JsonSerializable;
+
+class Entity implements JsonSerializable
+{
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this->toArray());
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+}
--- src/Lib/Table.php
+++ src/Lib/Table.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace PHPhademic\Lib;
+
+interface Table
+{
+    public function create(Entity $entity): Entity;
+
+    public function get(int $id): Entity;
+
+    public function find(array $parameters): array;
+
+    public function update(Entity $entity): bool;
+
+    public function delete(int $id): bool;
+}
