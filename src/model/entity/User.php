dcb5709fc0c4c45e3870989b7864ecf156cca7f0
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-12-01 22:39:32 -0300

    ADD user

---
+++ src/model/entity/User.php
@@ -0,0 +1,15 @@
+<?php
+
+class Artwork
+{
+    public int $id;
+    public string $username;
+    public string $email;
+    public string $name;
+    public string $avatar;
+    public DateTime $created;
+    public DateTime $updated;
+}
+
+
+?>
