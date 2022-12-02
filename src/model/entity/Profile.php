4bce1b83aaf0508b58224191354da6778bf643bb
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-12-01 21:59:53 -0300

    ADD entidade profile

---
+++ src/model/entity/Profile.php
@@ -0,0 +1,13 @@
+<?php
+
+class Profile{
+    public int $id;
+    public int $userId;
+    public string $name;
+    public string $avatar;
+    public DateTime $created;
+    public DateTime $updated;
+}
+
+
+?>
